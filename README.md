fd7b42edf52dcbf3f5621e19933a5ee98031425a
BrunoMine <borgesdossantosbruno@gmail.com>
2018-11-09 17:05:27 -0200

    Ajustes

--- README.md
+++ README.md
@@ -12,6 +12,19 @@ Jogo ao estilo jogos vorazes
 * Minetest 0.5.0 ou superior
 * Mod intllib (opicional)
 
+## Configurações avançadas
+É possivel reconfigurar o loot de itens padrão adicionando a seguinte linha em `minetest.conf`
+```conf
+battle_set_item_loot_<itemstring trocando o ':' por '_'> = <raridade 0.000 a 1.000> <preciosidade 1 a 10> <intervalo de quantidades> <intervalo de desgaste para ferramentas>
+# Exemplos
+# Cuidado com os espaços e os traços para intervalos (min-max)
+# use disable para desativar o loot do item
+# Escreva nil nos trechos que não vai usar
+battle_set_item_loot_farming_bread = 1.000 4 98-99 nil
+battle_set_item_loot_3d_armor_helmet_wood = 0.001 1 nil 1000-65000
+battle_set_item_loot_3d_armor_helmet_diamond = disable
+```
+
 ## Licença
 Veja LICENSE.txt para informações detalhadas da licença LGPL 3.0
 
--- loot_items.lua
+++ loot_items.lua
@@ -28,12 +28,44 @@ battle.reset_loot = function()
 end
 
 -- Verifica se mod treasurer está ativo para continuar
-if not minetest.get_modpath("treasurer") then
+if battle.loot_status == true and not minetest.get_modpath("treasurer") then
+	minetest.log("error", "[Battle] Sistema de loot ativado mas inoperante por falta do mod treasurer")
 	return 
 end
 
+-- Pega configuração fornecida pelo arquivo minetest.conf para editar itens especificos registrados por padrão
+-- Exemplo: battle_set_item_loot_farming_bread = 0.600 4 1-10 nil
+local get_loot_item_settings = function(name)
+	local st = minetest.settings:get("battle_set_item_loot_"..string.gsub(name, ":", "_"))
+	if not st then return false end
+	if st == "disable" then return true end
+	local d = st:split" "
+	d = {
+		["1"] = tonumber(d[1]),
+		["2"] = tonumber(d[2]),
+		["3"] = d[3],
+		["4"] = d[4]
+	}
+	if d["3"] ~= "nil" then 
+		d["3"] = d["3"]:split"-"
+		d["3"][1] = tonumber(d["3"][1])
+		d["3"][2] = tonumber(d["3"][2])
+	else
+		d["3"] = nil
+	end
+	if d["4"] ~= "nil" then 
+		d["4"] = d["4"]:split"-"
+		d["4"][1] = tonumber(d["4"][1])
+		d["4"][2] = tonumber(d["4"][2])
+	else
+		d["4"] = nil
+	end
+	treasurer.register_treasure(name, d["1"], d["2"], d["3"], d["4"])
+	return true
+end
+
 -- Carregar tesouros padrões
-if minetest.settings:get("battle_load_default_loot_itens") ~= "true" then
+if minetest.settings:get("battle_load_default_loot_itens") ~= "false" then
 	for _,d in ipairs({
 	--	itemstring			raridade	preciosidade	qtd min e max	desgaste para ferramentas min e max
 	--	"modname:item"			0.001 ~ 1.000	1 ~ 10		1 ~ 99		0 ~ 65535
@@ -46,7 +78,78 @@ if minetest.settings:get("battle_load_default_loot_itens") ~= "true" then
 		{"default:sword_mese",		0.080,		8,		nil,		{1000,65000}	},
 		{"default:sword_diamond",	0.020,		10,		nil,		{1000,65000}	},
 	}) do
-		treasurer.register_treasure(unpack(d))
+		local d_set = get_loot_item_settings(d[1])
+		if d_set == nil then
+			treasurer.register_treasure(unpack(d))
+		end
+	end
+end
+
+-- Carrega armaduras
+-- Verifica se mod treasurer está ativo para continuar
+if minetest.get_modpath("3d_armor") 
+	and minetest.settings:get("battle_load_armor_loot_itens") ~= "false" 
+then
+	for _,d in ipairs({
+	--	itemstring				raridade	preciosidade	qtd min e max	desgaste para ferramentas min e max
+	--	"modname:item"				0.001 ~ 1.000	1 ~ 10		1 ~ 99		0 ~ 65535
+		{"3d_armor:helmet_wood",		0.700,		1,		nil,		{1000,65000}	},
+		{"3d_armor:chestplate_wood",		0.700,		1,		nil,		{1000,65000}	},
+		{"3d_armor:leggings_wood",		0.700,		1,		nil,		{1000,65000}	},
+		{"3d_armor:boots_wood",			0.700,		1,		nil,		{1000,65000}	},
+		
+		{"3d_armor:helmet_cactus",		0.600,		1,		nil,		{1000,65000}	},
+		{"3d_armor:chestplate_cactus",		0.600,		1,		nil,		{1000,65000}	},
+		{"3d_armor:leggings_cactus",		0.600,		1,		nil,		{1000,65000}	},
+		{"3d_armor:boots_cactus",		0.600,		1,		nil,		{1000,65000}	},
+		
+		{"3d_armor:helmet_steel",		0.400,		4,		nil,		{1000,65000}	},
+		{"3d_armor:chestplate_steel",		0.400,		4,		nil,		{1000,65000}	},
+		{"3d_armor:leggings_steel",		0.400,		4,		nil,		{1000,65000}	},
+		{"3d_armor:boots_steel",		0.400,		4,		nil,		{1000,65000}	},
+		
+		{"3d_armor:helmet_bronze",		0.200,		7,		nil,		{1000,65000}	},
+		{"3d_armor:chestplate_bronze",		0.200,		7,		nil,		{1000,65000}	},
+		{"3d_armor:leggings_bronze",		0.200,		7,		nil,		{1000,65000}	},
+		{"3d_armor:boots_bronze",		0.200,		7,		nil,		{1000,65000}	},
+		
+		{"3d_armor:helmet_diamond",		0.100,		10,		nil,		{1000,65000}	},
+		{"3d_armor:chestplate_diamond",		0.100,		10,		nil,		{1000,65000}	},
+		{"3d_armor:leggings_diamond",		0.100,		10,		nil,		{1000,65000}	},
+		{"3d_armor:boots_diamond",		0.100,		10,		nil,		{1000,65000}	},
+		
+		{"3d_armor:helmet_gold",		0.100,		9,		nil,		{1000,65000}	},
+		{"3d_armor:chestplate_gold",		0.100,		9,		nil,		{1000,65000}	},
+		{"3d_armor:leggings_gold",		0.100,		9,		nil,		{1000,65000}	},
+		{"3d_armor:boots_gold",			0.100,		9,		nil,		{1000,65000}	},
+		
+		{"3d_armor:helmet_mithril",		0.100,		10,		nil,		{1000,65000}	},
+		{"3d_armor:chestplate_mithril",		0.100,		10,		nil,		{1000,65000}	},
+		{"3d_armor:leggings_mithril",		0.100,		10,		nil,		{1000,65000}	},
+		{"3d_armor:boots_mithril",		0.100,		10,		nil,		{1000,65000}	},
+		
+		{"3d_armor:helmet_crystal",		0.100,		10,		nil,		{1000,65000}	},
+		{"3d_armor:chestplate_crystal",		0.100,		10,		nil,		{1000,65000}	},
+		{"3d_armor:leggings_crystal",		0.100,		10,		nil,		{1000,65000}	},
+		{"3d_armor:boots_crystal",		0.100,		10,		nil,		{1000,65000}	},
+		
+		{"shields:shield_wood",			0.700,		10,		nil,		{1000,65000}	},
+		{"shields:shield_enhanced_wood",	0.600,		10,		nil,		{1000,65000}	},
+		{"shields:shield_cactus",		0.700,		10,		nil,		{1000,65000}	},
+		{"shields:shield_enhanced_cactus",	0.600,		10,		nil,		{1000,65000}	},
+		{"shields:shield_steel",		0.400,		10,		nil,		{1000,65000}	},
+		{"shields:shield_bronze",		0.200,		10,		nil,		{1000,65000}	},
+		{"shields:shield_diamond",		0.100,		10,		nil,		{1000,65000}	},
+		{"shields:shield_gold",			0.100,		9,		nil,		{1000,65000}	},
+		{"shields:shield_mithril",		0.100,		10,		nil,		{1000,65000}	},
+		{"shields:shield_crystal",		0.100,		10,		nil,		{1000,65000}	},
+	}) do
+		if minetest.registered_tools[d[1]] then
+			local d_set = get_loot_item_settings(d[1])
+			if d_set == false then
+				treasurer.register_treasure(unpack(d))
+			end
+		end
 	end
 end
 
@@ -63,15 +166,13 @@ local loot_nodes = {
 -- Altera on_rightclick
 local old_on_rightclick = {}
 for nn,loot in pairs(loot_nodes) do
-	if loot.status == true then
+	if battle.loot_status == true then
 		old_on_rightclick[nn] = minetest.registered_nodes[nn].on_rightclick
 		minetest.override_item(nn, {
 			on_rightclick = function(pos, node, player, itemstack, pointed_thing)
 				local meta = minetest.get_meta(pos)
-			
 				-- Verifica se ja foi carregado
 				if tonumber(meta:get_string("battle:loot_number")) ~= battle.loot_number_control then
-					
 					-- Verifica se existe esse tipo de node na tabela de loots
 					local amount = math.random(loot.qtd[1], loot.qtd[2])
 
@@ -87,7 +188,6 @@ for nn,loot in pairs(loot_nodes) do
 					-- Salva como carregado
 					meta:set_string("battle:loot_number", battle.loot_number_control)
 				end
-				
 				if old_on_rightclick[nn] then
 					return old_on_rightclick[nn](pos, node, player, itemstack, pointed_thing)
 				end
